275bb0665eb556721a5fa725a465347aceeee216
MOIS3Y <stepan@zhukovsky.me>
2023-03-26 14:57:28 +0900

    Fix check nixGL and guess_terminal if nixGL == 'False' Add NixOS logo

--- overlays/helpers/utils.py
+++ overlays/helpers/utils.py
@@ -1,22 +1,28 @@
 # █░█ ▀█▀ █ █░░ █▀
 # █▄█ ░█░ █ █▄▄ ▄█
 # ----------------
-
 import os
 from libqtile.utils import guess_terminal
 
 
 # nixGL apps:
 def nixGL(app):
-    if os.getenv('QTILE_NIXGL'):
+    nixgl = os.getenv('QTILE_NIXGL', 'False')
+    if nixgl == 'True':
         return "nixGL {}".format(app)
     return app
+
+def check_terminal():
+    terminal = os.getenv('QTILE_TERM', 'None')
+    if terminal == 'None':
+        return guess_terminal()
+    return terminal
 
 
 utils = {
     # Guess nixGL wrap utils:
     'browser': nixGL('firefox'),
-    'terminal': nixGL(os.getenv('QTILE_TERM', guess_terminal())),
+    'terminal': nixGL(check_terminal()),
     'telegram': nixGL('telegram-desktop'),
     # Normal utils:
     'mattermost': 'matermost-desktop',
